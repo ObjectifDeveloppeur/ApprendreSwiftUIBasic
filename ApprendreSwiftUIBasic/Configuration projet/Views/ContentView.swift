f35c814dd4c68271324baed42375dbeac688e6a5
Lathiere Maxime <lathiere.maxime@gmail.com>
2022-08-30 16:01:47 +0200

    full project

--- ApprendreSwiftUIBasic/Configuration projet/Views/ContentView.swift
+++ ApprendreSwiftUIBasic/Configuration projet/Views/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(Episode.all) { episode in
+                NavigationLink(episode.titre) {
+                    EpisodeDetailView(episode: episode)
+                }
+            }
+            .navigationTitle("Cours")
+            .navigationBarTitleDisplayMode(.large)
         }
-        .padding()
     }
 }
 
