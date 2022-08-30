6e221fdb0c091a46fa206f350465da4cde3e7691
Lathiere Maxime <lathiere.maxime@gmail.com>
2022-08-30 15:55:02 +0200

    Initial Commit

---
+++ ApprendreSwiftUIBasic/ApprendreSwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApprendreSwiftUIBasicApp.swift
+//  ApprendreSwiftUIBasic
+//
+//  Created by Maxime Lathiere on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApprendreSwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
